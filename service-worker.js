05afbcbb0bd2903118e404f194355baef6752839
maninder-singh-91 <manindersingh@.gofynd.com>
2026-09-01 23:11:51 +0530

    Fix iOS viewport resolution rendering

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='adaptive-glb-viewer-v2.19-bottom-sheet-r2';
+const CACHE='adaptive-glb-viewer-v2.19-ios-resolution-r1';
 const CORE=['./','./index.html','./assets/env_metal_001_d01c4504e0.hdr','./assets/models/ring02.glb'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
