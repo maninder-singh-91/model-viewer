5f4a29942e00f736368fc149861241aafc721d4f
maninder-singh-91 <manindersingh@.gofynd.com>
2026-09-03 15:56:44 +0530

    Refine diamond bloom and freeze viewer defaults

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='adaptive-glb-viewer-v2.19-diamond-bounces-4-r57';
+const CACHE='adaptive-glb-viewer-v2.19-frozen-diamond-preset-r66';
 const CORE=['./','./index.html','./assets/env-gem-4.exr','./assets/env_metal_001_d01c4504e0.hdr','./assets/models/ring02.glb'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
